3ef03611023347714d4697cc3d0590f43d28d073
unknown <fvcastellanos@gmail.com>
2020-05-19 20:51:35 -0600

    add initial html layout

---
+++ app-ui/src/app/assigment/assigment.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-assigment',
+  templateUrl: './assigment.component.html',
+  styleUrls: ['./assigment.component.css']
+})
+export class AssigmentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
